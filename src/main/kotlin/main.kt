f3af35aa3f6fc7fda927ba8096425af14137553b
lucas <lcs_demais@hotmail.com>
2023-01-20 20:18:00 -0300

    att

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+import kotlin.reflect.typeOf
+
+fun main(){
+     println("Hello, Sejam Bem Vindos ao ByteBank")
+
+    val titular = "Lucas Ramon"
+    val agencia = "1784-78"
+
+    println("Sr(a) $titular sua agência é $agencia")
+ }
