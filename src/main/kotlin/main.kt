9cd0a31764df9a9f82702c3da8324eca1075f0af
lucas <lcs_demais@hotmail.com>
2023-01-21 07:26:06 -0300

    att

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,13 @@
 import kotlin.reflect.typeOf
 
 fun main(){
-     println("Hello, Sejam Bem Vindos ao ByteBank")
+     println("Hello, Seja Bem Vindo(a) ao ByteBank")
 
     val titular = "Lucas Ramon"
-    val agencia = "1784-78"
+    val numeroConta = 1758
+    var saldo = 0.0
+    saldo += 28745.78
 
-    println("Sr(a) $titular sua agência é $agencia")
+    
+    println("Sr(a) $titular sua agência é $numeroConta com saldo de $saldo")
  }
