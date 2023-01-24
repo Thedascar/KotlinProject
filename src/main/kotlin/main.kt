17f5fdbfe4a09e641d7a3a0e79098bd19af77ba8
lucas <lcs_demais@hotmail.com>
2023-01-24 19:26:45 -0300

    att

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,12 +2,33 @@ fun main(){
     val Lucas = Funcionario(
         nome = "Lucas Ramon",
         cpf = "789.456.123-28",
-        salario = 1000.00,
-        tipo = 0
+        salario = 1000.00
     )
 
     println("Nome: ${Lucas.nome}")
     println("Cpf: ${Lucas.cpf}")
     println("Salario S/Bonus: ${Lucas.salario}")
     println( println("Salario C/Bonus: ${Lucas.bonificacao()}"))
+
+    println()
+    println()
+
+
+    val Aline = Gerente(
+        nome = "Aline Lima",
+        cpf = "123.456.789-10",
+        salario = 2000.00,
+        senha = 1234
+    )
+
+    println("Nome: ${Aline.nome}")
+    println("Cpf: ${Aline.cpf}")
+    println("Salario S/Bonus: ${Aline.salario}")
+    println( println("Salario C/Bonus: ${Aline.bonificacao()}"))
+
+    if(Aline.autenticacao(1234) == true){
+        println("Autenticado com sucesso")
+    }else{
+        println("Senha incorreta")
+    }
 }
